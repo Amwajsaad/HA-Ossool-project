b522010fdd0c154d93a9cf0288831eac4154e538
hayfalshareef99-star <hayfalshareef99@gmail.com>
2026-04-05 18:05:38 +0300

    .env file with the link and called dotnev library

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Dotenv from 'dotenv';
+
+Dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
